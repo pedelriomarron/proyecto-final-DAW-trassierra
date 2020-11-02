10521b0a0ac647dca7a0d25570517fc08bbdb0e3
Pedro Manuel del Río Marrón <pedelriomarron@gmail.com>
2020-11-02 20:01:46 +0100

    BOrrado theme

--- src/resources/views/includes/nav.blade.php
+++ src/resources/views/includes/nav.blade.php
@@ -1,3 +1,7 @@
+<!-- 
+
+
+
 @if (Route::has('login'))
 <div class="top-right links">
     @auth
@@ -92,39 +96,103 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto"></ul>
+<ul class="navbar-nav mr-auto"></ul>
 
+
+<ul class="navbar-nav ml-auto">
+    @guest
+    <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+    <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+    @else
+    <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
+    <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
+    <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li>
+    <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }} <span class="caret"></span>
+        </a>
+
+
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+        </div>
+    </li>
+    @endguest
+</ul>
+</div>
+</div>
+</nav>
+
+
+
+-->
+
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="./img/logo_white.svg" width="50vw" alt="Logo"> Vehiculos Electricos
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+</nav>
+<nav class="navbar navbar-expand-md navbar-light bg-light shadow-sm">
+    <div class="container">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
                 @guest
-                <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+                @endif
                 @else
-                <li><a class="nav-link" href="{{ route('users.index') }}">Manage Users</a></li>
-                <li><a class="nav-link" href="{{ route('roles.index') }}">Manage Role</a></li>
-                <li><a class="nav-link" href="{{ route('products.index') }}">Manage Product</a></li>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>Administrar</a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('users.index') }}">Administrar Usuarios</a>
+                        <a class="dropdown-item" href="{{ route('roles.index') }}">Administrar Roles</a>
+                        <a class="dropdown-item" href="{{ route('products.index') }}">Administrar Productos</a>
+                    </div>
+                </li>
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
+                        {{ Auth::user()->name }}
                     </a>
 
-
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
 
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
                         </form>
                     </div>
                 </li>
                 @endguest
+
             </ul>
         </div>
     </div>
